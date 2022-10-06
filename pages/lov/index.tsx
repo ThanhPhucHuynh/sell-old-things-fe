fa78ad0de7d1e895f15443ec0775ccfffc8637b9
Thanh Phuc Huynh <nguoigiaumat100@gmail.com>
2022-10-06 19:44:31 +0700

    update

--- pages/lov/index.tsx
+++ pages/lov/index.tsx
@@ -19,6 +19,8 @@ function truncate(
 }
 const LovComponent = () => {
   const [showModal, setShowModal] = useState(false);
+  const [finish, setFinish] = useState(false);
+
   const [token, setToken] = useState('');
   React.useEffect(() => {
     if ('serviceWorker' in navigator) {
@@ -50,16 +52,63 @@ const LovComponent = () => {
     }
     setTokenFunc();
   }, []);
+
   return (
     <>
       <div className="flex items-center justify-center h-60">
-        <button
-          className="px-6 py-3 text-purple-100 bg-purple-600 rounded-md"
-          type="button"
-          onClick={() => setShowModal(true)}
-        >
-          Open Modal {token}
-        </button>
+        {true ? (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <img
+              className="max-w-md h-auto"
+              src="https://media2.giphy.com/media/dYZxsY7JIMSy2Afy6e/giphy.gif?cid=ecf05e47asq1b2cujas5mgbjm1i53l1tfka38kgu54o17126&rid=giphy.gif&ct=g"
+              alt="description"
+            />
+            <button
+              className="px-6 py-3 text-purple-100 bg-purple-600 rounded-md"
+              type="button"
+              onClick={() => setShowModal(true)}
+            >
+              Bé nhấp vào đây nè
+            </button>
+          </div>
+        ) : (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <p>Em đợi nó load một tí nha!!3 </p>
+            <div role="status">
+              <svg
+                aria-hidden="true"
+                className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        )}
       </div>
       {showModal ? (
         <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -70,35 +119,32 @@ const LovComponent = () => {
           />
           <div className="flex items-center min-h-screen px-4 py-8">
             <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
-              <div className="mt-3 sm:flex">
-                <div className="flex items-center justify-center flex-none w-12 h-12 mx-auto bg-red-100 rounded-full">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-red-600"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+              <div className="mt-3">
+                <div className="flex items-center justify-center flex-none w-50 h-50 mx-auto bg-red-100 rounded-full">
+                  <img
+                    className="max-w-md h-auto"
+                    src={
+                      finish
+                        ? 'https://media4.giphy.com/media/l4FGzTDDUZ3OPorFS/giphy.gif?cid=ecf05e47asq1b2cujas5mgbjm1i53l1tfka38kgu54o17126&rid=giphy.gif&ct=g'
+                        : 'https://media0.giphy.com/media/xT9IgAWzvvcohEMofu/giphy.gif?cid=ecf05e47asq1b2cujas5mgbjm1i53l1tfka38kgu54o17126&rid=giphy.gif&ct=g'
+                    }
+                    alt="description"
+                  />
                 </div>
                 <div className="mt-2 text-center sm:ml-4 sm:text-left">
                   <h4 className="text-lg font-medium text-gray-800">
-                    Delete account ?
+                    {finish
+                      ? 'ròi á, em bấm đóng được rùi í. <3'
+                      : 'Copy cái này gửi anh nha? có cái nút ở dưới á. :3'}
                   </h4>
-                  {/* <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p> */}
+                  <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
+                    yêu em
+                  </p>
                   <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
                     {truncate(token)}
                   </p>
                   <div className="items-center gap-2 mt-3 sm:flex">
-                    <button
+                    {/* <button
                       type="button"
                       className="w-full mt-2 p-2.5 flex-1 text-white bg-red-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
                       onClick={() => setShowModal(false)}
@@ -111,16 +157,21 @@ const LovComponent = () => {
                       onClick={() => setShowModal(false)}
                     >
                       Cancel
-                    </button>
+                    </button> */}
                     <button
                       type="button"
                       className="w-full mt-2 p-2.5 flex-1 text-blue-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
                       onClick={() => {
-                        navigator.clipboard.writeText(token);
-                        setShowModal(false);
+                        if (finish) {
+                          window.close();
+                        } else {
+                          navigator.clipboard.writeText(token);
+                          // setShowModal(false);
+                          setFinish(true);
+                        }
                       }}
                     >
-                      copy
+                      {finish ? 'close' : 'copy'}
                     </button>
                   </div>
                 </div>
